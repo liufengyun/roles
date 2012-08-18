3374310b63211c3d5034a0566627268e968f768f
liufengyun <liufengyunchina@gmail.com>
2012-08-18 11:12:51 +0800

    remove mongoid code

--- lib/roles/railtie.rb
+++ lib/roles/railtie.rb
@@ -7,14 +7,6 @@ module Rolies
       ActiveSupport.on_load(:active_record) do
         ActiveRecord::Base.send :extend, Roles
       end
-      
-      config.before_initialize do
-        ::Mongoid::Document.module_eval do
-          def self.included(base)
-            base.extend Roles
-          end
-        end
-      end if defined?(Mongoid)
     end
   end
 end
